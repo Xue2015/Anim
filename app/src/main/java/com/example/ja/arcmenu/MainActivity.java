ca5b2ba0fad3e60eac3ea3a34210ba3b2a82bc67
JA <1161048063@qq.com>
2015-07-22 12:47:19 +0800

    將下拉的动画改成环形的动画

--- app/src/main/java/com/example/ja/arcmenu/MainActivity.java
+++ app/src/main/java/com/example/ja/arcmenu/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.ja.arcmenu;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnticipateInterpolator;
 import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -21,6 +29,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
             R.id.imageView_e, R.id.imageView_f, R.id.imageView_g, R.id.imageView_h,};
     private List<ImageView> imageViewList = new ArrayList<ImageView>();
     private boolean flag = true;
+    private int x0 = 0;
+    private int y0 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
             imageView.setOnClickListener(this);
             imageViewList.add(imageView);
         }
+//        int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+//        x0 = screenWidth;
+//        int screenHeight = getWindowManager().getDefaultDisplay().getHeight();
+//        y0 = screenHeight;
     }
 
     @Override
@@ -76,24 +90,68 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void closeAnim() {
         for (int i = 1; i < res.length; i++) {
-            ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i),
-                    "translationY", i * 200, 0F);
-            animator.setDuration(500);
-            animator.setInterpolator(new BounceInterpolator());
-//            animator.setStartDelay(i * 300);
-            animator.start();
+            double theta = (2 * Math.PI / 7) * (i - 1);
+            float x1 = (float) (x0 + 300 * Math.cos(theta));
+            float y1 = (float) (y0 + 300 * Math.sin(theta));
+            ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageViewList.get(i),
+                    "translationY", y1, 0F);
+            ObjectAnimator animator2 = ObjectAnimator.ofFloat(imageViewList.get(i),
+                    "translationX", x1, 0F);
+            AnimatorSet set = new AnimatorSet();
+            set.playTogether(animator1, animator2);
+            set.setDuration(500);
+            set.setInterpolator(new AnticipateInterpolator());
+            set.start();
+
+//            ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i),
+//                    "translationY", i * 200, 0F);
+//            animator.setDuration(500);
+//            animator.setInterpolator(new BounceInterpolator());
+////            animator.setStartDelay(i * 300);
+//            animator.start();
         }
         flag = true;
     }
 
+
     private void startAnim() {
         for (int i = 1; i < res.length; i++) {
-            ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i),
-                    "translationY", 0F, i * 200);
-            animator.setDuration(500);
-            animator.setInterpolator(new OvershootInterpolator());
-//            animator.setStartDelay(i * 300);
-            animator.start();
+            double theta = (2 * Math.PI / 7) * (i - 1);
+            float x1 = (float) (x0 + 300 * Math.cos(theta));
+            float y1 = (float) (y0 + 300 * Math.sin(theta));
+            ObjectAnimator animator1 = ObjectAnimator.ofFloat(imageViewList.get(i),
+                    "translationY", 0F, y1);
+            ObjectAnimator animator2 = ObjectAnimator.ofFloat(imageViewList.get(i),
+                    "translationX", 0F, x1);
+            AnimatorSet set = new AnimatorSet();
+            set.playTogether(animator1, animator2);
+            set.setDuration(500);
+            set.setInterpolator(new OvershootInterpolator());
+            set.start();
+//            double theta = (2 * Math.PI/7) * (i-1);
+//            float x1 = (float) (x0 + 100 * Math.cos(theta));
+//            float y1 = (float) (y0 + 200 * Math.sin(theta));
+//            Path path = new Path();
+//            int xe = (int) x1;
+//            int ye = (int) y1;
+//            path.moveTo(x0,y0);
+//            path.lineTo(xe,ye);
+//            ObjectAnimator animator = ObjectAnimator.ofFloat(imageViewList.get(i),
+//                    "translationX",x0,x1);
+//            PropertyValuesHolder p1 = PropertyValuesHolder.ofFloat("translationX",x0,x1);
+//            PropertyValuesHolder p2 = PropertyValuesHolder.ofFloat("translationY",y0,y1);
+//            ObjectAnimator.ofPropertyValuesHolder(imageViewList.get(i),p1,p2).setDuration(500).start();
+//            animator.setDuration(500);
+//            animator.setInterpolator(new OvershootInterpolator());
+////            animator.setStartDelay(i * 300);
+//            animator.start();
+//            ObjectAnimator.ofFloat(imageViewList.get(i),"translationX",x0,x1).start();
+//            ObjectAnimator animator2 = ObjectAnimator.ofFloat(imageViewList.get(i),"translationY",y0,y1);
+//
+//            AnimatorSet set = new AnimatorSet();
+//            set.playTogether(animator1,animator2);
+//            set.setDuration(500);
+//            set.start();
         }
         flag = false;
 
